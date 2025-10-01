9aac029c579d8a3556a1021084bf32ee88431a76
orange <da.naranjo10@uniandes.edu.co>
2025-09-30 19:28:56 -0500

    first general components

--- itaca/src/app/app.ts
+++ itaca/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   //template: '<h1>Hola Mundo!, desde {{ city }} con html as string</h1>',
   styleUrl: './app.css'
@@ -12,3 +14,4 @@ export class App {
   city = 'Bogota'
   protected readonly title = signal('itaca');
 }
+
