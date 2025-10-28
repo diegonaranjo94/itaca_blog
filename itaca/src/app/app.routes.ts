a7590a0fd2fd442fe2a406e4f62d63bf9d610cb0
orange <da.naranjo10@uniandes.edu.co>
2025-10-27 21:31:03 -0500

    general components and navigation

--- itaca/src/app/app.routes.ts
+++ itaca/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { AboutComponent } from './about/about';
+import { PortfolioComponent } from './portfolio/portfolio';
+import { BlogComponent } from './blog/blog';
+import { ContactComponent } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent }
+];
---
+++ itaca/src/app/blog/blog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.html',
+  styleUrls: ['./blog.css'],
+  standalone: true
+})
+export class BlogComponent {}
